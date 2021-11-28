d38f72896b241849dfa58ca47b15f4dda5d89678
overfly <1765074348@qq.com>
2021-11-28 14:43:58 +0800

    初始化线性表函数实现

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <assert.h>
+#include <malloc.h>
+#include <stdlib.h>
 
 #define SEQ_INIT_SIZE 10    //初始化顺序表容量
 #define SEQ_INC_SIZE  2     //顺序表增长因子
@@ -11,10 +14,30 @@ typedef struct
     int sursize;      //元素的个数
 }SeqList;
 
+//初始化线性表函数实现
+void InitSeqList(SeqList * plist);
+
 int main(void)
 {
     SeqList myseq;
 
+    InitSeqList(&myseq);
+
     printf("Hello, World!\n");
     return 0;
+}
+
+void InitSeqList(SeqList * plist)
+{
+    assert(plist != NULL);
+
+    plist->data = (ElemType *)malloc(sizeof(ElemType) * SEQ_INIT_SIZE);
+    if (NULL == plist)
+    {
+        printf("Failed to allocate memory");
+        exit(EXIT_FAILURE);
+        //若 exit_code 为 EXIT_FAILURE ，则返回指示不成功终止的实现定义状态
+    }
+    plist->sursize = 0;
+    plist->capacity = SEQ_INIT_SIZE;
 }
