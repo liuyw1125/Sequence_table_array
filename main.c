51178e40d5f7278c0be987a3d2f3abcca1142347
overfly <1765074348@qq.com>
2021-11-28 14:06:59 +0800

    定义了一个顺序表的结构

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+#define SEQ_INIT_SIZE 10    //初始化顺序表容量
+#define SEQ_INC_SIZE  2     //顺序表增长因子
+
+typedef int ElemType;
+typedef struct
+{
+    ElemType * data;  //存储空间地址
+    int capacity;     //容量
+    int sursize;      //元素的个数
+}SeqList;
+
 int main(void)
 {
+    SeqList myseq;
+
     printf("Hello, World!\n");
     return 0;
 }
